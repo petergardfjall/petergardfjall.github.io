e8a7370715a71eba5d6743e8f8bc899aef03b13b
petergardfjall <peter.gardfjall.work@gmail.com>
2019-02-09 17:15:30 +0100

    add css for use with emacs markdown-preview-mode

--- js/emacs-markdown/github-markdown-block-highlight.js
+++ js/emacs-markdown/github-markdown-block-highlight.js
@@ -1,7 +1,5 @@
-<script>
-    $(document).on('mdContentChange', function() {
-        $('pre code').each(function(i, block) {
-            hljs.highlightBlock(block);
-        });
+$(document).on('mdContentChange', function() {
+    $('pre code').each(function(i, block) {
+        hljs.highlightBlock(block);
     });
-</script>
+});
